ef6aa2c67fc6c90ce768e514f290184b992098a8
Chad Swatsworth <Chad Swatsworth>
2017-02-02 09:18:38 -0500

    x replaced 5, also true

--- isbn_exercise.rb
+++ isbn_exercise.rb
@@ -1,7 +1,11 @@
-def valid_digit_length(num)#<<num is a string '1234...'	
+def valid_ispn_length?(num)#<<num is a string '1234...'	
+				#passing in string, want a boolean returned
+				#boolean, true or false.
+		
 		if num.length == 10
 			true
 		else 
-			false 
+			false
+
 		end
 end
--- test_isbn_exercise.rb
+++ test_isbn_exercise.rb
@@ -8,13 +8,15 @@ class TestIsbnFunction < Minitest::Test
 
 	def test_returns_true
 			num = '1234567890'
-			assert_equal(true, valid_digit_length(num))
+			assert_equal(true, valid_ispn_length?(num))
 	end
 	def test_returns_false
 			num = '123456789'
-			assert_equal(false, valid_digit_length(num))
-	def test
-		
+			assert_equal(false, valid_ispn_length?(num))
 	end
+	def test_x_replaced_5_still_true_anything_with_length_of_10
+			#it is still true becuase x is counted in the string
+			num = '1234x67890'
+			assert_equal(true, valid_ispn_length?(num))
 	end
 end
